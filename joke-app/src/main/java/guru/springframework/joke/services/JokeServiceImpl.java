efba659523534f48a694ef330579d26509d1bcc6
ozzman02 <osantamaria@gmail.com>
2017-12-25 17:24:03 -0600

    Creating the Spring MVC Controller

--- joke-app/src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
+++ joke-app/src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
@@ -1,7 +1,10 @@
 package guru.springframework.joke.services;
 
+import org.springframework.stereotype.Service;
+
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
+@Service
 public class JokeServiceImpl implements JokeService {
 
 	private final ChuckNorrisQuotes chuckNorrisQuotes;
