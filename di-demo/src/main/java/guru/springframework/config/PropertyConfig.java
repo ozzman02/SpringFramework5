5b04ddc364cff068a21310f6c779c2a602cf2635
ozzman02 <osantamaria@gmail.com>
2017-12-31 14:29:58 -0600

    Spring Environment Properties

--- di-demo/src/main/java/guru/springframework/config/PropertyConfig.java
+++ di-demo/src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,10 +1,12 @@
 package guru.springframework.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import guru.springframework.examplebeans.FakeDataSource;
 
@@ -21,10 +23,14 @@ public class PropertyConfig {
 	@Value("${guru.url}")
 	private String url;
 	
+	@Autowired
+	Environment env;
+	
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
-		fakeDataSource.setUsername(username);
+		//fakeDataSource.setUsername(username);
+		fakeDataSource.setUsername(env.getProperty("GURU_USERNAME"));
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setUrl(url);
 		return fakeDataSource;
