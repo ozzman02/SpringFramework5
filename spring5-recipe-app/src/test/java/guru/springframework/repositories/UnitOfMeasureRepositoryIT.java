4addda84f73fc15f283995680bdb70b02b3ac645
ozzman02 <osantamaria@gmail.com>
2018-01-07 00:56:00 -0600

    Spring Integration Test

---
+++ spring5-recipe-app/src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,43 @@
+package guru.springframework.repositories;
+
+import static org.junit.Assert.*;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import guru.springframework.domain.UnitOfMeasure;
+
+/*
+ * Integration Test
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+	@Autowired
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+	
+	@Before
+	public void setUp() throws Exception {
+	
+	}
+
+	@Test
+	public void findByDescriptionTeaspoonTest() throws Exception {
+		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+		assertEquals("Teaspoon", uomOptional.get().getDescription());
+	}
+	
+	@Test
+	public void findByDescriptionCupTest() throws Exception {
+		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+		assertEquals("Cup", uomOptional.get().getDescription());
+	}
+
+}
