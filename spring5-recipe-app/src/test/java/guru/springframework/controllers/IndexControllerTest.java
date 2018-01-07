0daa2b37d9ba5f2b87d67b5e09b9cdba33b797e5
ozzman02 <osantamaria@gmail.com>
2018-01-07 00:35:09 -0600

    Mockito Argument Capture

--- spring5-recipe-app/src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ spring5-recipe-app/src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -3,12 +3,17 @@ package guru.springframework.controllers;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 
 public class IndexControllerTest {
@@ -30,17 +35,48 @@ public class IndexControllerTest {
 		
 	}
 
+	@SuppressWarnings("unchecked")
 	@Test
 	public void getIndexPage() throws Exception {
 		
+		/*
+		 * TDD ----> Given, When, Then
+		 */
+		
+		/*
+		 * Given
+		 */
+		Recipe recipe1 = new Recipe();
+		recipe1.setId(1L);
+		
+		Recipe recipe2 = new Recipe();
+		recipe2.setId(2L);
+		
+		Set<Recipe> recipes = new HashSet<Recipe>();
+		recipes.add(recipe1);
+		recipes.add(recipe2);
+		
+		when(recipeService.getRecipes()).thenReturn(recipes);
+		
+		ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+		
+		/*
+		 * When
+		 */
 		String viewName = indexController.getIndexPage(model);
 		
+		/*
+		 * Then
+		 */
 		assertEquals("index", viewName);
 		
 		verify(recipeService, times(1)).getRecipes();
 		
-		verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+		verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
 		
+		Set<Recipe> setInController = argumentCaptor.getValue();
+		
+		assertEquals(2, setInController.size());
 	}
 
 }
