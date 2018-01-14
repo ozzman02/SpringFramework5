3e0e1fe817b181675b008862a025875d602bed88
ozzman02 <osantamaria@gmail.com>
2018-01-14 00:30:02 -0600

    Display all recipe properties

--- spring5-recipe-app/src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ spring5-recipe-app/src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -1,11 +1,10 @@
 package guru.springframework.services;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import org.junit.Before;
@@ -31,6 +30,19 @@ public class RecipeServiceImplTest {
 		recipeService = new RecipeServiceImpl(recipeRepository);
 	}
 
+	@Test
+	public void getRecipesByIdTest() throws Exception {
+		
+		Recipe recipe = new Recipe();
+		
+		recipe.setId(1L);
+		
+		Optional<Recipe> recipeOptional = Optional.of(recipe);
+		
+		when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+		
+	}
+	
 	@Test
 	public void getRecipes() {
 		
