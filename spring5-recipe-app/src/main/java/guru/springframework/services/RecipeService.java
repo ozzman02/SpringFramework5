8f652f37f66fd41444d2a49a6547a35771752c3c
ozzman02 <osantamaria@gmail.com>
2018-01-16 20:42:57 -0600

    Delete a recipe

--- spring5-recipe-app/src/main/java/guru/springframework/services/RecipeService.java
+++ spring5-recipe-app/src/main/java/guru/springframework/services/RecipeService.java
@@ -14,5 +14,7 @@ public interface RecipeService {
 	RecipeCommand saveRecipeCommand(RecipeCommand recipeCommand);
 	
     RecipeCommand findCommandById(Long l);
+    
+    void deleteById(Long id);
 
 }
