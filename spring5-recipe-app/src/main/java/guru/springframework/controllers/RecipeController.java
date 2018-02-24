58655321f2d3b6371129bab5648216f24a58d600
ozzman02 <osantamaria@gmail.com>
2018-02-24 09:58:31 -0600

    Enumeration Dropdowns with Spring MVC and Thymeleaf

--- spring5-recipe-app/src/main/java/guru/springframework/controllers/RecipeController.java
+++ spring5-recipe-app/src/main/java/guru/springframework/controllers/RecipeController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
@@ -16,48 +15,46 @@ import lombok.extern.slf4j.Slf4j;
 public class RecipeController {
 
 	private final RecipeService recipeService;
-	
+
 	public RecipeController(RecipeService recipeService) {
 		this.recipeService = recipeService;
 	}
-	
-	@GetMapping
-	@RequestMapping("/recipe/{id}/show")
-    public String showById(@PathVariable String id, Model model){
-        model.addAttribute("recipe", recipeService.findById(new Long(id)));
-        return "recipe/show";
-    }
 
-	@GetMapping
-    @RequestMapping("recipe/new")
-    public String newRecipe(Model model){
-        model.addAttribute("recipe", new RecipeCommand());
-        return "recipe/recipeform";
-    }
+	@GetMapping("/recipe/{id}/show")
+	public String showById(@PathVariable String id, Model model) {
 
-    @GetMapping
-    @RequestMapping("recipe/{id}/update")
-    public String updateRecipe(@PathVariable String id, Model model){
-        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-        return  "recipe/recipeform";
-    }
+		model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
-    @PostMapping
-    @RequestMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
-        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
-        return "redirect:/recipe/" + savedCommand.getId() + "/show";
-    }
-    
-    @GetMapping
-    @RequestMapping("recipe/{id}/delete")
-    public String deleteById(@PathVariable String id) {
-    	
-    	log.debug("Deleting id: " + id);
-    	
-    	recipeService.deleteById(Long.valueOf(id));
-    	
-    	return "redirect:/";
-    }
-	
+		return "recipe/show";
+	}
+
+	@GetMapping("recipe/new")
+	public String newRecipe(Model model) {
+		model.addAttribute("recipe", new RecipeCommand());
+
+		return "recipe/recipeform";
+	}
+
+	@GetMapping("recipe/{id}/update")
+	public String updateRecipe(@PathVariable String id, Model model) {
+		model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+		return "recipe/recipeform";
+	}
+
+	@PostMapping("recipe")
+	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+		return "redirect:/recipe/" + savedCommand.getId() + "/show";
+	}
+
+	@GetMapping("recipe/{id}/delete")
+	public String deleteById(@PathVariable String id) {
+
+		log.debug("Deleting id: " + id);
+
+		recipeService.deleteById(Long.valueOf(id));
+		return "redirect:/";
+	}
+
 }
