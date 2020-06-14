f9b5ea64bbfca982b39a5653674bc8ff6defd48a
Oscar Santamaria <oscarsantamaria@OSCAR-SANTAMARIA.local>
2020-06-13 22:50:28 -0600

    introduction to router functions

--- spring5-reactive-mongo-recipe-app/src/main/java/guru/springframework/commands/IngredientCommand.java
+++ spring5-reactive-mongo-recipe-app/src/main/java/guru/springframework/commands/IngredientCommand.java
@@ -3,9 +3,10 @@ package guru.springframework.commands;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.validator.constraints.NotBlank;
+
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
---
+++ spring5-reactive-mongo-recipe-app/src/main/java/guru/springframework/config/WebConfig.java
@@ -0,0 +1,29 @@
+package guru.springframework.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.services.RecipeService;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
+@Configuration
+public class WebConfig {
+	
+	private static final String GET_RECIPES_ROUTE = "/api/recipes";
+	
+	@Bean
+	RouterFunction<?> routes(RecipeService recipeService) {
+		return RouterFunctions.route(GET(GET_RECIPES_ROUTE), 
+				serverRequest -> ServerResponse
+					.ok()
+					.contentType(MediaType.APPLICATION_JSON)
+					.body(recipeService.getRecipes(), Recipe.class));
+	}
+	
+}
